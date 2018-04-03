c9bfa7c469a1f5ef15e34dbb1a00acf4e84328f8
DESKTOP-DR1ICBV\Jake <jakepolson71@gmail.com>
2018-04-03 04:51:17 -0400

    4/3/2018 4:50am

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,6 +3,25 @@
 #include "TankPlayerController.h"
 #include "BattleTank.h"
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Not Controlling a Tank!"));
+	}
+
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Controlling %s."), (*ControlledTank->GetName()));
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
